1bb99552d8dc8cf524984a9e22b216f1e93a5138
julfelder <julfelder@wustl.edu>
2016-04-27 18:11:42 -0500

    Update student-report.js

--- js/student-report.js
+++ js/student-report.js
@@ -5,7 +5,7 @@ function print(message) {
   document.write("<section>" + message + "</section>")
 }
 
-for(var i=0; i<students.length;i++;) {
+for(var i=0; i<students.length;i++) {
   student= students[i];
   message += '<h2>'+ student.name +'</h2>';
 }
